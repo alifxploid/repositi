b3510d3ac601f35d5be9e7e589b1f28320f05e49
alifxploid <69151185+alifxploid@users.noreply.github.com>
2022-12-15 17:32:49 +0700

    Add files via upload

---
+++ batang.js
@@ -0,0 +1,10 @@
+<script>
+	atOptions = {
+		'key' : '29eece38e5769b66a44412bdc5faa12e',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://heartachegrabbedlaunching.com/29eece38e5769b66a44412bdc5faa12e/invoke.js"></scr' + 'ipt>');
+</script>
